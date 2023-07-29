0a08c26076a6b3d41b9b8fca3fb683b3606815a6
RyanJHamby <68158512+RyanJHamby@users.noreply.github.com>
2023-07-29 14:18:36 -0400

    Fix errors in app rendering on browser

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
+import MainPage from "./pages/MainPage";
+import EmissionsInfoPage from "./pages/EmissionsInfoPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
-    errorElement: <div>Not found!</div>,
+    element: <MainPage />,
+    errorElement: <ErrorPage />,
+  },
+    {
+    path: "/emissions-info",
+    element: <EmissionsInfoPage />,
+    errorElement: <ErrorPage />,
   },
 ]);
+
+// export default function routes() {
+//   return <RouterProvider
+//             router={router}
+//   />
+// }
